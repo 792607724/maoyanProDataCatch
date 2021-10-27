69c02e43d5d357fa53b6d800ab874b15bae711cc
chen guangtao <39899119+792607724@users.noreply.github.com>
2021-10-27 17:41:25 +0800

    Begin coding……

--- common.py
+++ common.py
@@ -2,6 +2,8 @@
 import os
 import re
 
+from airtest.core.error import AdbShellError
+
 os.path.abspath(".")
 """
     @File:common.py
@@ -16,11 +18,20 @@ class Common:
         self.device = device
         self.poco = poco
 
+    def install_apk(self, package_path="./apk/maoyanPro.apk"):
+        print("begin install app:{}".format(package_path))
+        self.device.install_app(filepath=package_path, replace=True)
+
     def grantPermission(self, package_name="com.sankuai.moviepro"):
-        permission_list = self.device.shell(
-            "dumpsys package {} | grep permission | grep granted=false".format(package_name))
-        permission_list = re.findall("\s*(.*):\sgranted", permission_list)
-        print("current app:{} has permission:[{}]".format(package_name, permission_list))
-        for permission in permission_list:
-            self.device.shell("pm grant {} {}".format(package_name, permission))
-            print("Now, grant app {} - permission {}".format(package_name, permission))
+        try:
+            permission_list = self.device.shell(
+                "dumpsys package {} | grep permission | grep granted=false".format(package_name))
+            permission_list = re.findall("\s*(.*):\sgranted", permission_list)
+            print("current app:{} has permission:[{}]".format(package_name, permission_list))
+            if permission_list:
+                for permission in permission_list:
+                    self.device.shell("pm grant {} {}".format(package_name, permission))
+                    print("Now, grant app {} - permission {}".format(package_name, permission))
+        except AdbShellError:
+            print("No need granted for this permission!")
+
--- function.py
+++ function.py
@@ -18,10 +18,73 @@ cur_time = time.strftime("%Y%m%d_%H%M%S")
     @Date:2021/10/27
 """
 
+
+class Function:
+
+    def __init__(self, device, poco):
+        self.device = device
+        self.poco = poco
+        self.package_name = "com.sankuai.moviepro"
+        self.package_path = "./apk/maoyanPro.apk"
+        self.guide_name = "同意并继续"
+        self.function_name = "排片上座"
+
+    def launch_maoyanPro(self):
+        self.device.start_app(self.package_name)
+
+    def skip_guide(self):
+        try:
+            self.poco(text=self.guide_name).wait().click()
+        except Exception as ex:
+            print("No need skip guide: exception\n {}".format(str(ex)))
+
+    def enter_function(self):
+        try:
+            self.poco(text=self.function_name).wait().click()
+        except Exception as ex:
+            print("No need do this, check your code exception\n {}".format(str(ex)))
+
+    def get_current_date(self):
+        try:
+            current_date = self.poco("com.sankuai.moviepro:id/tv_date").wait().get_text()
+            print(current_date)
+            return current_date
+        except Exception as ex:
+            print("No need do this, check your code exception\n {}".format(str(ex)))
+
+    def wait_to_goal_date(self, date="2018年4月21日"):
+        try:
+            self.poco("com.sankuai.moviepro:id/tv_date").wait().click()
+            print("Please choose your date by manually:\n{}".format(date))
+            while True:
+                try:
+                    choose_date = self.get_current_date()
+                    print("Waiting…… now please scroll to find specific date……")
+                    if choose_date == date:
+                        print("Date choose correct, start catch data")
+                        return True
+                    sleep(1)
+                except Exception:
+                    continue
+        except Exception as ex:
+            print("No need do this, check your code exception\n {}".format(str(ex)))
+
+
 if __name__ == '__main__':
     print("Running test……")
-    package_name = "com.sankuai.moviepro"
+
     device = connect_device("Android:///{}".format("7c2440fd"))
     poco = AndroidUiautomationPoco(device=device, use_airtest_input=False, screenshot_each_action=False)
+
+    function = Function(device, poco)
+
     common = Common(device, poco)
-    common.grantPermission(package_name)
+    # common.install_apk(function.package_path)
+    # common.grantPermission(function.package_name)
+
+    function.launch_maoyanPro()
+    # function.skip_guide()
+    function.enter_function()
+    function.get_current_date()
+    if function.wait_to_goal_date():
+        print("Begin")
