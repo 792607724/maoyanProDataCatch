077fdaa37129f973bb60e20301c9adbf6d9fa6d5
chen guangtao <39899119+792607724@users.noreply.github.com>
2021-10-29 20:12:54 +0800

    Final version

---
+++ temp.py
@@ -0,0 +1,14 @@
+# coding = utf8
+import os
+
+os.path.abspath(".")
+"""
+    @File:temp.py
+    @Author:Bruce
+    @Date:2021/10/29
+"""
+
+if __name__ == '__main__':
+    os.system("adb devices")
+
+
