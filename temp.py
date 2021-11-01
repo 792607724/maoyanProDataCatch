8e2dfeaa6452dc6e5db7e1fbcd49cd14b86440db
Bruce <39899119+792607724@users.noreply.github.com>
2021-11-01 14:12:16 +0800

    update windows

--- temp.py
+++ temp.py
@@ -1,6 +1,8 @@
 # coding = utf8
 import os
 
+from rich import print
+
 os.path.abspath(".")
 """
     @File:temp.py
@@ -8,7 +10,15 @@ os.path.abspath(".")
     @Date:2021/10/29
 """
 
+
+def hello():
+    print("OK")
+
+
 if __name__ == '__main__':
     os.system("adb devices")
-
-
+    print("OK,[bold magenta]That is so exciting[/bold magenta]!", ":vampire:", locals(), "\n this is bullshit")
+    os.system("adb uninstall com.netease.nie.yosemite")
+    os.system("adb uninstall com.netease.open.pocoservice.test")
+    os.system("adb uninstall com.netease.open.pocoservice")
+    os.system("adb uninstall com.sankuai.moviepro")
